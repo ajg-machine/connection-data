2f996a745da86cb515b4504fe60d647e3b92b29f
ajg-machine <109889463+ajg-machine@users.noreply.github.com>
2023-11-20 04:26:24 -0500

    Update own-server-read.js

--- api/own-server-read.js
+++ api/own-server-read.js
@@ -1,15 +1,15 @@
 async function getOwnServerIp(key) {
-    let targetUrl = "https://api.github.com/repos/ajg-machine" +
-        "/connection-data/contents/own-server";
-    let headers = { accept: "application/vnd.github.raw" };
-    let response = await fetch(targetUrl, { headers });
-    let data = await response.text();
-    data = Uint8Array.from(atob(data), c => c.charCodeAt(0));
-    key = await crypto.subtle.importKey(
-        "raw", key, { name: "AES-GCM" }, false, ["encrypt", "decrypt"]
+    let response = await fetch(
+        "https://api.github.com/repos/ajg-machine/connection-data/contents/own-server",
+        { headers: { accept: "application/vnd.github.raw" } });
+    let data = Uint8Array.from(atob(await response.text()), c => c.charCodeAt(0));
+    data = await crypto.subtle.decrypt(
+        { name: "AES-GCM", length: 256, iv: data.slice(0, 12) },
+        await crypto.subtle.importKey(
+            "raw", key, { name: "AES-GCM" }, false, ["encrypt", "decrypt"]
+        ),
+        data.slice(12)
     );
-    let algorithm = { name: "AES-GCM", length: 256, iv: data.slice(0, 12) };
-    data = await crypto.subtle.decrypt(algorithm, key, data.slice(12));
     data = (new TextDecoder()).decode(data);
     return JSON.parse(data)["public-ip"];
 }
